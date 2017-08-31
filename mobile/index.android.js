183d97ca10d00c3f9f69db2ce3eb82b939890efd
EQuimper <quimperemanuel@gmail.com>
2017-08-31 19:45:21 -0400

    Init commit

---
+++ mobile/index.android.js
@@ -0,0 +1,8 @@
+// @flow
+
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+
+AppRegistry.registerComponent('findmeamovie', () => App);
